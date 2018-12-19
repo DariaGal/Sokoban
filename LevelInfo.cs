526eac2a0860fb5e3555801f137f407d2bf46fa2
Daria Galiullina <dariaa.gal@yandex.com>
2018-12-19 16:52:10 +0700

    Add LevelInfo

---
+++ LevelInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    class LevelInfo
+    {
+        public string Name { get; }
+        public Map Map { get; }
+        Position Player { get; }
+        public List<Position> Goal { get; }
+    }
+}
