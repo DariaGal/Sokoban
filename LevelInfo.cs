b794ea6c66eeb79acba7c4482709831549f60021
Daria Galiullina <dariaa.gal@yandex.com>
2018-12-19 17:32:01 +0700

    Add map size to LevelInfo

--- LevelInfo.cs
+++ LevelInfo.cs
@@ -17,6 +17,8 @@ namespace Sokoban
 
         public LevelInfo(string name, ICell[,] map, Position player, List<Position> goals)
         {
+            MapWidth = map.GetLength(0);
+            MapHeight= map.GetLength(1);
             Name = name;
             Map = map;
             PlayerPosition = player;
